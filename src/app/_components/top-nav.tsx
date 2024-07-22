c91a861fb6d162a55f9170c856201998af485a8b
Nahuel Chaves <nahuel.chaves@gmail.com>
2024-07-22 19:00:53 -0300

    update

---
+++ src/app/_components/top-nav.tsx
@@ -0,0 +1,21 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const TopNav = () => {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Dettagli</div>
+      <div>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+            <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  )
+}
+
+export default TopNav
