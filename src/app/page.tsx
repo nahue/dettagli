f7eca0559feb77a8e4e07ef96e488bf9d3f18e07
Nahuel Chaves <nahuel.chaves@gmail.com>
2024-07-22 18:21:23 -0300

    scaffold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/7f2aaba6-cb86-4b27-9983-38bda24a38c8-tvktp9.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/7f3ad45e-a364-4f6a-8668-6440043faaa2-e37z8c.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/814f9d54-974f-449d-a377-062928225be9-7qvlt8.jpg",
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Dettagli
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
