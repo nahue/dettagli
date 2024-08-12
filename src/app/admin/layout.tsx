8d5c3435d1c782234d8d18663b50afd680c22299
Nahuel Chaves <nahuel.chaves@gmail.com>
2024-08-12 00:27:00 -0300

    admin force-dynamic

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import type { Metadata } from "next";
 import AppSidebar from "./_components/Sidebar";
 
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: "Next Shadcn Dashboard Starter",
   description: "Basic dashboard with Next.js and Shadcn",
 };
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
